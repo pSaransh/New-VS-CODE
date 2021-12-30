4da9ea9f3e302b7d2d7166a0c8156650faba1aee
Ramansh Pathak <ramansh.38cptn@gmail.com>
2021-12-31 02:02:32 +0530

    .

--- Passgen/index.js
+++ Passgen/index.js
@@ -6,33 +6,28 @@ import savePassword from './utils/savePassword.js'
 import clipboardy from './node_modules/clipboardy/index.js'
 const log = console.log
 commander.version('1.0.0').description('Simple Password Generator')
-// commander
-//     .command('generate')
-//     .action(()=>{
-//         console.log('Generated');
-//     })
-//     .parse()
 commander
     .option('-l, --length <number>',
     'length of password'
     ,'8')
+    .option('--msg <string>',
+    'save password with its details',
+    '20')
     .option('-s, --save',
-    'save password to password.txt')
+    'save password to password.csv')
     .option('-nn, --no-number',
     'remove numbers')
     .option('-ns, --no-symbols',
     'remove symbols')
     .parse()
 
-const {length,save,numbers,symbols} = commander.opts()
+const {length,msg,save,numbers,symbols} = commander.opts()
 // get generated password
 const generatedPassword = createPassword(length,numbers,symbols)
 
 // save to file
-if(save){
-    savePassword(generatedPassword)
-    chalk.green(log('Password saved to passwords.txt'))
-}
+if(save)
+    savePassword(generatedPassword,msg)
 // copy to clipboard
 clipboardy.writeSync(generatedPassword)
 
