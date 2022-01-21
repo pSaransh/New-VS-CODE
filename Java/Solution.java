c4d70de27899d71ff74e0277f3bcc7c483d235ce
Ramansh Pathak <ramansh.38cptn@gmail.com>
2022-01-21 19:20:14 +0530

    Okay

---
+++ Java/Solution.java
@@ -0,0 +1,21 @@
+package Java;
+import java.util.*;
+public class Solution {
+
+   public static void main(String[] args) {
+        List<Integer> list=new ArrayList<Integer>();  
+        int[] arr = {515, 341, 98, 44, 211};
+        for(int i:arr) list.add(i);
+        new Solution().reorderDigits(list,"asc");
+       
+   }
+   void reorderDigits(List<Integer> list,String s)
+   {
+       if(s=="asc"){
+           
+       }
+       else{
+
+       }
+   }
+}
