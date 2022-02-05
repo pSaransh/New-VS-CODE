977e282714dfafc83e082cdf5176d7d2736a56e3
Ramansh Pathak <ramansh.38cptn@gmail.com>
2022-02-05 22:44:07 +0530

    .

---
+++ Java/Regular_Expressions.java
@@ -0,0 +1,51 @@
+// String s = "His life was supposed to be simple but there's just one attribute that he's proud of. He was born in Hidden Leaf Village. His parents are shinobi and was killed in Nine-Tail Attack, the day on which a child of prophecy was born-Naruto. With Naruto he also grow up as a father, as a guardian. Although he was just a teacher of Naruto but he meant so much for Naruto. During childhood of Naruto, he always looked up for him and help him to become a better person. He and Ichiraku-Ramen were the only shield that keep Naruto away from all the hate imposed by villagers. He cried when the same people who used to hate Naruto, calles him \"Hero of the Hidden Leaf\", his eyes were filled with tears and there were several flashbacks running through his mind. It matters that Naruto's struggle is important but what's most important that Naruto always have someone on his side. His name is Iruka Umino.";
+package Java;
+import java.util.*;
+public class Regular_Expressions {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        /*
+        Regular Expressions - to check for a specific pattern in the given text or search some text from a given file.
+        \d - [0-9]
+        \D - [^0-9]
+        \s - [ \t\n\f\r]
+        \S - [^ \t\n\f\r]
+        \w - [a-zA-Z0-9_]
+        \W - [^\w]
+        * - zero or more
+        + - one or more
+        ^ - begin with | not included
+        $ - end with
+        ? - optional
+        {n,} - at least n times
+        {n,m} - at least n times, atmost m times
+        */
+        //to check digits
+        String digits = "\\d+";
+        boolean flag = false;
+        do{
+            System.out.println("Input numbers: ");
+            String number = sc.next();
+            flag = number.matches(digits);
+            if(!flag)
+                System.out.println("Invalid");
+            else
+                System.out.println("Valid");
+        }
+        while(!flag);
+
+        //to check date
+        String dateCheck = "(3?[0-1]|[0-2][0-9])-(0[0-9]|1[1-2])-([1-2][0-9]{3})";
+        do{
+            System.out.println("Input date: ");
+            String number = sc.next();
+            flag = number.matches(dateCheck);
+            if(!flag)
+                System.out.println("Invalid");
+            else
+                System.out.println("Valid");
+        }
+        while(!flag);
+        sc.close();
+    }   
+}
--- Java/Solution.java
+++ Java/Solution.java
@@ -1,43 +1,23 @@
 package Java;
-import java.util.*;
-public class Solution {
-    
+
+/**
+ * Solution
+**/
+class Solution{
+    private String name;
+    private int id;
+    private String city;
+    Solution(String city){
+        this.city = city;
+    }
+    Solution(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
     public static void main(String[] args) {
-        ArrayList<Integer> newList = new ArrayList<Integer>();
-        for(int i=1 ; i<=10 ; i++)
-            newList.add(i);
-        StringBuffer newString = new StringBuffer();
-        for(int i:newList)
-            newString.append(i);
-        System.out.println(newString.toString());
-    }
-    int newCondition(int n,int k){
-        int count=0;
-        for(int i=1; i<=n ;i++){
-            /**
-            If the following condition pass then add result from getTotalDivisions() to count.
-            **/
-            if(i%k==0){
-                int result = getTotalDivisions(i, k, 0);
-                count+=result;
-            }
-        }
-        return count;
-    }
-    static int getTotalDivisions(int n, int k,int count)
-    {
-        /**
-        This recursive function will calculate all the divisions possible between two numbers
-        For Ex: n = 4 and k = 2
-        It'll return 2 because 2 can divide 4, 2 times.
-        For Ex: n = 6 and k = 2
-        It'll return one.
-        Rest is self-explanatory
-        **/
-        if(n<k) return count;
-        else if(n%k==0){
-            return getTotalDivisions(n/k, k, count+1);
-        }
-        return count;        
+        Solution e1 = new Solution("NYC");
+        Solution e2 = new Solution("Victor Karel",2345);
+        System.out.println(e1.name+" "+e1.city+" "+e1.id);
+        System.out.println(e2.name+" "+e2.city+" "+e2.id);
     }
 }
