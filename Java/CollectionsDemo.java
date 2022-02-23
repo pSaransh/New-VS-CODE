fd26e24a5144ee4eca55410fbb26581c54d34ca0
Ramansh Pathak <ramansh.38cptn@gmail.com>
2022-02-24 00:56:22 +0530

    .

---
+++ Java/CollectionsDemo.java
@@ -0,0 +1,14 @@
+package Java;
+import java.util.*;
+public class CollectionsDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = sc.nextInt();
+        
+        
+    }
+}
---
+++ Java/VarArgs.java
@@ -0,0 +1,25 @@
+package Java;
+public class VarArgs {
+    public static void main(String[] args){
+        A obj1 = new A();
+        System.out.println(obj1.add(2,3,345,767,23,1324,56));
+        B obj2 = new B()
+                    {
+                        public void show(){
+                            System.out.println("Hello World");
+                        }
+                    };
+        obj2.show();
+
+    }
+}
+class A{
+    public int add(int... b){
+        int sum=0;
+        for(int i:b) sum+=i;
+        return sum;
+    }
+}
+interface B{
+    void show();
+}
